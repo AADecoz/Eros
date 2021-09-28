411ccd72e45738299af775f9fff8bc2997a402fe
AADecoz <82182504+AADecoz@users.noreply.github.com>
2021-09-28 11:39:08 +0200

    gen comp profile

--- datingsite/src/app/profile/profile.component.ts
+++ datingsite/src/app/profile/profile.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  username:string|null = localStorage.getItem('username');
+  password = localStorage.getItem('password');
+  sex:any = localStorage.getItem('sex');
+  preference:any = localStorage.getItem('preference');
+  age:any = localStorage.getItem('age');
+  area:any = localStorage.getItem('area');
+  intro:any= localStorage.getItem('intro');
+
   constructor() { }
 
   ngOnInit(): void {
